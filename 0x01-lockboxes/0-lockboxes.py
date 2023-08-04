72e4ffbf07c421576b173a8eb5b9a4669c77abdf
Festus Omaku <fomaku3@gmail.com>
2023-08-04 04:06:21 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,17 +10,19 @@ def canUnlockAll(boxes):
             bool: True if all boxes can be opened, False otherwise.
     """
 
-
+    # Initialize the current index to 0 and create a set containing box 0
     index = 0
     total = list(set(boxes[0])| {0})
     added = True
     while added:
         added = False
+        # Iterate through keys in the current box and add them to the set
         for j in join(boxes,total[index:]):
             if j not in total:
                 total.append(j)
                 index +=1
                 added= True
     print(total)
-    
+
+    # Return True if the number of opened boxes is equal to the total number of boxes
     return len(total)==len(boxes)
