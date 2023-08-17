aee8a8b48c34525dcd6167c511a1c91b623f03ae
Festus Omaku <fomaku3@gmail.com>
2023-08-17 19:33:42 +0100

    deep fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,65 +1,57 @@
 #!/usr/bin/python3
-""" module computes a class and prints its metrics """
+"""Log parsing"""
 
 
 import sys
-import datetime
+import re
+import signal
 
 
-class LogStats:
-    """
-        A class to compute and print metrics from log data.
-    """
-
-    def __init__(self):
-        """
-            Initialize the LogStats instance with initial metrics.
-        """
-
-        self.total_size = 0
-        self.status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                             404: 0, 405: 0, 500: 0}
-        self.line_count = 0
-
-    def process_line(self, line):
-        """
-            Process a single log line and update the metrics.
-            Args:
-                line (str): The log line to process.
-        """
-
-        parts = line.split()
-        if len(parts) < 7:
-            return
-        ip_address = parts[0]
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        self.total_size += file_size
-        if status_code in self.status_codes:
-            self.status_codes[status_code] += 1
-
-        self.line_count += 1
-        if self.line_count % 10 == 0:
-            self.print_stats()
-
-    def print_stats(self):
-        """
-            Print the current metrics.
-        """
-
-        print(f"File size: {self.total_size}")
-        for code, count in sorted(self.status_codes.items()):
-            if count > 0:
-                print(f"{code}: {count}")
-                print()
+def print_statistics(total_size, status_counts):
+    """Prints the statistics"""
+    print("Total file size: {}".format(total_size))
+    for code, count in sorted(status_counts.items()):
+        if count > 0:
+            print("{}: {}".format(code, count))
 
 
-if __name__ == "__main__":
-    stats = LogStats()
-    try:
-        for line in sys.stdin:
-            stats.process_line(line.strip())
-    except KeyboardInterrupt:
-        stats.print_stats()
-        sys.exit(0)
+log_pattern = re.compile(
+        r"([0-9]+(?:\.[0-9]+){3}) - \[([^\]]+)\] \"GET \/projects\/260 HTTP\/1\.1\" ([0-9]+) ([0-9]+)")
+
+total_size = 0
+status_counts = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
+line_counter = 0
+
+
+def interrupt_handler(signum, frame):
+    """Handles KeyboardInterrupt and prints statistics before exiting."""
+    print_statistics(total_size, status_counts)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
+try:
+    for line in sys.stdin:
+        match = re.match(log_pattern, line.strip())
+        if match:
+            _, _, status, size = match.groups()
+            total_size += int(size)
+            if status in status_counts:
+                status_counts[status] += 1
+            line_counter += 1
+            if line_counter == 10:
+                print_statistics(total_size, status_counts)
+                line_counter = 0
+except KeyboardInterrupt:
+    print_statistics(total_size, status_counts)
+    sys.exit(0)
